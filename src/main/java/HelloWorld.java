8c68dceacbe05aa4e36ff934575c8e3f492188c0
JJ Ma <jonathan.j.ma@espn.com>
2016-07-26 12:19:15 -0400

    added extra print statement

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -4,6 +4,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.err.println("hello world");
+        System.out.println("extra hello world");
     }
 
     public int add (int var1, int var2) {

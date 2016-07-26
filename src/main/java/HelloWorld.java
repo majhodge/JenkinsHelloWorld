0de3179475f60ebe4ff7a34261da407c7b64ac77
JJ Ma <jonathan.j.ma@espn.com>
2016-07-26 11:10:36 -0400

    added source files

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,24 @@
+/**
+ * Created by majo on 7/26/16.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+
+    public int add (int var1, int var2) {
+        return var1 + var2;
+    }
+
+    public int div (int var1, int var2) {
+        return var1 / var2;
+    }
+
+    public int sub (int var1, int var2) {
+        return var1 - var2;
+    }
+
+    public int mult (int var1, int var2) {
+        return var1 * var2;
+    }
+}

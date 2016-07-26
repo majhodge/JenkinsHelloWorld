673a67f2aa0e3a8b5e71cfe6a84d080892463f55
JJ Ma <jonathan.j.ma@espn.com>
2016-07-26 15:41:58 -0400

    changed err to out

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -3,7 +3,7 @@
  */
 public class HelloWorld {
     public static void main(String[] args) {
-        System.err.println("hello world");
+        System.out.println("hello world");
     }
 
     public int add (int var1, int var2) {

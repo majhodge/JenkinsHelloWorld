0f756780e6f83e1fbdfdc603d27400caae6ec4ea
JJ Ma <jonathan.j.ma@espn.com>
2016-07-27 18:29:15 -0400

    cobertura test

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -5,17 +5,17 @@ import static org.junit.Assert.*;
  */
 public class HelloWorldTest {
 
-//    @org.junit.Test
-//    public void testAdd() throws Exception {
-//        HelloWorld helloWorld = new HelloWorld();
-//        assertEquals(4, helloWorld.add(1,3));
-//    }
-//
-//    @org.junit.Test
-//    public void testDiv() throws Exception {
-//        HelloWorld helloWorld = new HelloWorld();
-//        assertEquals(2, helloWorld.div(8,4));
-//    }
+    @org.junit.Test
+    public void testAdd() throws Exception {
+        HelloWorld helloWorld = new HelloWorld();
+        assertEquals(4, helloWorld.add(1,3));
+    }
+
+    @org.junit.Test
+    public void testDiv() throws Exception {
+        HelloWorld helloWorld = new HelloWorld();
+        assertEquals(2, helloWorld.div(8,4));
+    }
 
     @org.junit.Test
     public void testSub() throws Exception {

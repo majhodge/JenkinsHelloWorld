084d8a32ba3e7c5940f205129a5932621432049b
JJ Ma <jonathan.j.ma@espn.com>
2016-07-28 16:45:01 -0400

    modified readme

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -12,27 +12,27 @@ public class HelloWorldTest {
         System.out.println("starting tests!");
     }
 
-//    @org.junit.Test
-//    public void testAdd() throws Exception {
-//        HelloWorld helloWorld = new HelloWorld();
-//        assertEquals(4, helloWorld.add(1,3));
-//    }
-//
-//    @org.junit.Test
-//    public void testDiv() throws Exception {
-//        HelloWorld helloWorld = new HelloWorld();
-//        assertEquals(2, helloWorld.div(8,4));
-//    }
-//
-//    @org.junit.Test
-//    public void testSub() throws Exception {
-//        HelloWorld helloWorld = new HelloWorld();
-//        assertEquals(4, helloWorld.sub(7,3));
-//    }
-//
-//    @org.junit.Test
-//    public void testMult() throws Exception {
-//        HelloWorld helloWorld = new HelloWorld();
-//        assertEquals(3, helloWorld.mult(1,3));
-//    }
+    @org.junit.Test
+    public void testAdd() throws Exception {
+        HelloWorld helloWorld = new HelloWorld();
+        assertEquals(4, helloWorld.add(1,3));
+    }
+
+    @org.junit.Test
+    public void testDiv() throws Exception {
+        HelloWorld helloWorld = new HelloWorld();
+        assertEquals(2, helloWorld.div(8,4));
+    }
+
+    @org.junit.Test
+    public void testSub() throws Exception {
+        HelloWorld helloWorld = new HelloWorld();
+        assertEquals(4, helloWorld.sub(7,3));
+    }
+
+    @org.junit.Test
+    public void testMult() throws Exception {
+        HelloWorld helloWorld = new HelloWorld();
+        assertEquals(3, helloWorld.mult(1,3));
+    }
 }

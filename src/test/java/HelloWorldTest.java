0186e032c3e1b5933377fae4b5349561d2d715ad
JJ Ma <jonathan.j.ma@espn.com>
2016-07-28 14:51:20 -0400

    increased coverage

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -18,21 +18,21 @@ public class HelloWorldTest {
         assertEquals(4, helloWorld.add(1,3));
     }
 
-//    @org.junit.Test
-//    public void testDiv() throws Exception {
-//        HelloWorld helloWorld = new HelloWorld();
-//        assertEquals(2, helloWorld.div(8,4));
-//    }
-//
-//    @org.junit.Test
-//    public void testSub() throws Exception {
-//        HelloWorld helloWorld = new HelloWorld();
-//        assertEquals(4, helloWorld.sub(7,3));
-//    }
-//
-//    @org.junit.Test
-//    public void testMult() throws Exception {
-//        HelloWorld helloWorld = new HelloWorld();
-//        assertEquals(3, helloWorld.mult(1,3));
-//    }
+    @org.junit.Test
+    public void testDiv() throws Exception {
+        HelloWorld helloWorld = new HelloWorld();
+        assertEquals(2, helloWorld.div(8,4));
+    }
+
+    @org.junit.Test
+    public void testSub() throws Exception {
+        HelloWorld helloWorld = new HelloWorld();
+        assertEquals(4, helloWorld.sub(7,3));
+    }
+
+    @org.junit.Test
+    public void testMult() throws Exception {
+        HelloWorld helloWorld = new HelloWorld();
+        assertEquals(3, helloWorld.mult(1,3));
+    }
 }

c26bcbb310b72daf953e75cc44aafdee63b2a014
JJ Ma <jonathan.j.ma@espn.com>
2016-08-03 16:40:37 -0400

    should pass here

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -9,7 +9,7 @@ public class HelloWorldTest {
 
     @Before
     public void setup() throws Exception {
-        System.out.println("Hello");
+        System.out.println("Hello!");
     }
 
     @org.junit.Test

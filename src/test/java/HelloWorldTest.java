b1a681f131980a2b2a02139d1654f13eb227b5a0
JJ Ma <jonathan.j.ma@espn.com>
2016-08-08 17:10:52 -0400

    brought code coverage all the way down to zero

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,41 +1,37 @@
-
-
 import static org.junit.Assert.*;
 import org.junit.*;
-
-
 
 /**
  * Created by majo on 7/26/16.
  */
 public class HelloWorldTest {
 
-    @Before
-    public void setup() throws Exception {
-        System.out.println("Hello!");
-    }
-
-    @Test
-    public void testAdd() throws Exception {
-        HelloWorld helloWorld = new HelloWorld();
-        assertEquals(4, helloWorld.add(1,3));
-    }
-
-    @Test
-    public void testDiv() throws Exception {
-        HelloWorld helloWorld = new HelloWorld();
-        assertEquals(2, helloWorld.div(8,4));
-    }
-
-    @Test
-    public void testSub() throws Exception {
-        HelloWorld helloWorld = new HelloWorld();
-        assertEquals(4, helloWorld.sub(7,3));
-    }
-
-    @Test
-    public void testMult() throws Exception {
-        HelloWorld helloWorld = new HelloWorld();
-        assertEquals(3, helloWorld.mult(1,3));
-    }
+//    @Before
+//    public void setup() throws Exception {
+//        System.out.println("Hello!");
+//    }
+//
+//    @Test
+//    public void testAdd() throws Exception {
+//        HelloWorld helloWorld = new HelloWorld();
+//        assertEquals(4, helloWorld.add(1,3));
+//    }
+//
+//    @Test
+//    public void testDiv() throws Exception {
+//        HelloWorld helloWorld = new HelloWorld();
+//        assertEquals(2, helloWorld.div(8,4));
+//    }
+//
+//    @Test
+//    public void testSub() throws Exception {
+//        HelloWorld helloWorld = new HelloWorld();
+//        assertEquals(4, helloWorld.sub(7,3));
+//    }
+//
+//    @Test
+//    public void testMult() throws Exception {
+//        HelloWorld helloWorld = new HelloWorld();
+//        assertEquals(3, helloWorld.mult(1,3));
+//    }
 }

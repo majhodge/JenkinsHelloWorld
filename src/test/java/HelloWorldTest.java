200897bc4d420a20f0c2108872340f2eec6a3c5c
JJ Ma <jonathan.j.ma@espn.com>
2016-08-08 17:44:25 -0400

    increased code coverage across all methods

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,29 +1,29 @@
 import static org.junit.Assert.*;
-
+import org.junit.*;
 /**
  * Created by majo on 7/26/16.
  */
 public class HelloWorldTest {
 
-    @org.junit.Test
+    @Test
     public void testAdd() throws Exception {
         HelloWorld helloWorld = new HelloWorld();
         assertEquals(4, helloWorld.add(1,3));
     }
 
-    @org.junit.Test
+    @Test
     public void testDiv() throws Exception {
         HelloWorld helloWorld = new HelloWorld();
         assertEquals(2, helloWorld.div(8,4));
     }
 
-    @org.junit.Test
+    @Test
     public void testSub() throws Exception {
         HelloWorld helloWorld = new HelloWorld();
         assertEquals(4, helloWorld.sub(7,3));
     }
 
-    @org.junit.Test
+    @Test
     public void testMult() throws Exception {
         HelloWorld helloWorld = new HelloWorld();
         assertEquals(3, helloWorld.mult(1,3));

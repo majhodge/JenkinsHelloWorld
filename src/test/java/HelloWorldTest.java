5f9c05c065498d9da3320707b5783e3478a69440
JJ Ma <jonathan.j.ma@espn.com>
2016-08-08 09:13:45 -0400

    changed imports in unit tests

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,6 +1,9 @@
-import org.junit.Before;
+
 
 import static org.junit.Assert.*;
+import org.junit.*;
+
+
 
 /**
  * Created by majo on 7/26/16.
@@ -12,25 +15,25 @@ public class HelloWorldTest {
         System.out.println("Hello!");
     }
 
-    @org.junit.Test
+    @Test
     public void testAdd() throws Exception {
         HelloWorld helloWorld = new HelloWorld();
         assertEquals(4, helloWorld.add(1,3));
     }
 
-    @org.junit.Test
+    @Test
     public void testDiv() throws Exception {
         HelloWorld helloWorld = new HelloWorld();
         assertEquals(2, helloWorld.div(8,4));
     }
 
-    @org.junit.Test
+    @Test
     public void testSub() throws Exception {
         HelloWorld helloWorld = new HelloWorld();
         assertEquals(4, helloWorld.sub(7,3));
     }
 
-    @org.junit.Test
+    @Test
     public void testMult() throws Exception {
         HelloWorld helloWorld = new HelloWorld();
         assertEquals(3, helloWorld.mult(1,3));
